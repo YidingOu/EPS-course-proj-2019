ea0b85a4a1408690b7cee142eb667dbaec0f44e0
yang <yang@10.0.0.188>
2019-04-03 15:29:41 -0400

    FIX CORS issue // not sure if works

--- ipv/src/main/java/com/ipv/rest/UserRestAPI.java
+++ ipv/src/main/java/com/ipv/rest/UserRestAPI.java
@@ -3,6 +3,7 @@ package com.ipv.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import com.ipv.exception.NotFoundException;
 import com.ipv.service.UserService;
 import com.ipv.wrapper.ValidateResponseWapper;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/users")
 public class UserRestAPI {
