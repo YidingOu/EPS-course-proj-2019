1571e59dc5f851ae184fff050af39bb8ccb4cd09
yang <yang@10.0.0.188>
2019-04-04 15:46:37 -0400

    upload the script for db

---
+++ ipv/script/initial_script.sql
@@ -0,0 +1,3 @@
+INSERT INTO `pivdb`.`User` (`Name`, `DisplayName`, `Role`, `Pass`, `Salt`) VALUES ('admin', 'admin', '2', 'admin', 'salt1');
+INSERT INTO `pivdb`.`User` (`Name`, `DisplayName`, `Role`, `Pass`, `Salt`) VALUES ('staff', 'staff', '1', 'staff', 'salt2');
+INSERT INTO `pivdb`.`User` (`Name`, `DisplayName`, `Role`, `Pass`, `Salt`) VALUES ('user', 'user', '0', 'user', 'salt3');
