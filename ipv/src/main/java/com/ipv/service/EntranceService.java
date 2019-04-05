6893d133d127d48b1d9ea979bb8c0604e5641d3e
yang <yang@10.0.0.188>
2019-04-05 19:39:31 -0400

    Reverse change

--- ipv/src/main/java/com/ipv/service/EntranceService.java
+++ ipv/src/main/java/com/ipv/service/EntranceService.java
@@ -3,12 +3,13 @@ package com.ipv.service;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class EntranceService {
-
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-}
+//disabled now
+//@Controller
+//public class EntranceService {
+//
+////    @GetMapping("/")
+////    public String index() {
+////        return "index";
+////    }
+//
+//}
