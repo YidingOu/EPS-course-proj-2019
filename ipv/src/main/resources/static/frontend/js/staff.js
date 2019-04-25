2ba21db06bc187dfdb4e03ead41d4e0375135c8d
ngruixin <ng.ruixin@gmail.com>
2019-04-25 17:15:19 -0400

    updated admin staff page

--- ipv/src/main/resources/static/frontend/js/staff.js
+++ ipv/src/main/resources/static/frontend/js/staff.js
@@ -1,3 +1,5 @@
+var STAFF = 1;      //Constant used to represent a staff user
+
 $(document).ready(function () {
     getStaff();
 });
@@ -11,7 +13,6 @@ function getStaff() {
         type: "GET",
         contentType: "application/json",
         url: "/users",
-        // data: JSON.stringify(form_data),
         dataType: 'json',
         cache: false,
         timeout: 600,
@@ -19,26 +20,57 @@ function getStaff() {
             console.log("success");
             console.log(data);
 
-            var role_map = {
-                0: 'User',
-                1: 'Staff',
-                2: 'Admin'
-            };
-
             var content_html = '';
             for (var i = 0; i < data.length; i++) {
-                content_html += '<td> <a href=\'staff1.html?id=' + data[i].id + ' \'> ' + data[i].name + ' </a></td>' + '\n';
-                content_html += '<td> ' + role_map[data[i].role] + ' </td>' + '\n';
+                if (data[i].role != STAFF) continue;
+                content_html += '<td> <a href=\'staff1.html?id=' + data[i].id + ' \'> ' + data[i].firstName + " " + data[i].lastName + ' </a></td>' + '\n';
+                content_html += '<td> ' + data[i].name + ' </td>' + '\n';
+                content_html += '<td id="staff' + data[i].id + '"> </td>' + '\n';
                 content_html += '</tr>' + '\n';
+                populateCases(data[i].id);
             }
-
             document.getElementById("staff-table").innerHTML = content_html;
 
         },
         error: function (e) {
+            console.log("fail");
             console.log(e);
-            alert("Alert.");
+            alert("Error, please refresh the page. ");
         }
     });
-
 }
+
+function populateCases(staffId) {
+    console.log("/posts/by_staff/" + staffId);
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "/posts/by_staff/" + staffId,
+        dataType: 'json',
+        cache: false,
+        timeout: 600,
+        success: function (data) {
+            console.log("success");
+            var usernames = [];
+            for (var i=0; i<data.length; i++) {
+                var user = data[i].user;
+                if (user != null) {
+                    usernames.push(user.name);
+                }
+            }
+            console.log(usernames);
+            if (usernames.length == 0) {
+                $("#staff" + staffId).html('None');
+            } else {
+                $("#staff" + staffId).html(usernames.toString());
+            }
+
+        },
+        error: function (e) {
+            console.log(e);
+            alert("Error, please refresh the page. ");
+        }
+    });
+}
+
+//  function arrToStrin
