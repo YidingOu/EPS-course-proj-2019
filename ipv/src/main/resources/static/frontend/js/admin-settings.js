4f96554e09f9ca281e462668b61d9cc8ae1f1fd8
ngruixin <ng.ruixin@gmail.com>
2019-04-26 14:39:51 -0400

    fixes on admin

--- ipv/src/main/resources/static/frontend/js/admin-settings.js
+++ ipv/src/main/resources/static/frontend/js/admin-settings.js
@@ -1,20 +1,18 @@
 uid = null
 PWD_LEN = 1 //TODO: change after testing
-staffId = null;
+users = {}
 
 $(document).ready(function() {
     validate();
+    $("#save-btn").click(function() {
+        saveChanges();
+    });
     $("#logout").click(function() {
         logout();
     });
     uid = getUid();
-    staffId = getStaffId();
     init();
 });
-
-function getStaffId() {
-    return window.location.hash.substring(1) ? (window.location.hash.substring(1)) : uid;
-}
 
 /** Performs validation of profile update form, ensures that username
  *  and password fields are not empty, and that passwords provided match.
@@ -48,17 +46,26 @@ function validate() {
         minlength: "Your password must be at least 8 characters long"
       },
       password_cfm: "Passwords must match!"
-    },
-    submitHandler: saveChanges()
+    }
   });
 };
 
+//TODO: is there a reason why we do this
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+        .exec(window.location.search);
+
+    return (results !== null) ? results[1] || 0 : false;
+}
+
 /** Populates the first name, last name, username fields of the current user */
 function init() {
+    //var id = $.urlParam('id');
+    console.log("/users/" + uid);
     $.ajax({
         type: "GET",
         contentType: "application/json",
-        url: "/users/" + staffId,
+        url: "/users/" + uid,
         dataType: 'json',
         cache: false,
         timeout: 600,
@@ -86,6 +93,43 @@ function getUid() {
     return;
 }
 
+/** Performs validation of profile update form, ensures that username
+ *  and password fields are not empty, and that passwords provided match.
+ */
+function validate() {
+  $("#admin-profile").validate({
+    rules: {
+      first_name: {
+        required: true
+      },
+      last_name: {
+        required: true
+      },
+      username: {
+        required: true
+      },
+      password: {
+        required: true,
+        minlength: PWD_LEN
+      },
+      password_cfm: {
+        equalTo: "#password"
+      }
+    },
+    messages: {
+      last_name: "Please provide a last name",
+      first_name: "Please provide a first name",
+      username: "Please provide a username",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 8 characters long"
+      },
+      password_cfm: "Passwords must match!"
+    },
+    submitHandler: saveChanges()
+  });
+};
+
 /** Checks to ensure valid fields for a profile update:
  *  (i) username, first name, last name, password fields are not empty
  *  (ii) password is the same as the confirmed password
@@ -100,18 +144,17 @@ function validFields() {
 
 /** Sends an ajax request to save account changes */
 function saveChanges() {
-    $("#staff-profile").submit(function(event){
+    $("#admin-profile").submit(function(event){
         if (!validFields()) return;
-        event.preventDefault(); //prevent default action
         var post_url = "/users/update_pass"
         var request_method = "PUT";
-        var main_url = "/frontend/src/admin/staff.html";
+        var main_url = "/frontend/src/admin/dashboard.html";
         var form_data = {
             firstName:$("#first_name").val(),
             lastName:$("#last_name").val(),
             pass:$("#password").val(),
             name:$("#username").val(),
-            id:staffId
+            id:uid
         };
         console.log(form_data);
 
@@ -124,7 +167,7 @@ function saveChanges() {
             cache: false,
             timeout: 60000,
             success: function (data) {
-                alert("Profile successfully updated!")
+                console.log("success");
                 $(location).attr("href", main_url);
             },
             error: function (e) {
@@ -133,46 +176,7 @@ function saveChanges() {
                 alert("Your profile failed to update, please try again.")
             }
         });
-    });
-};
-
-/** Sends an ajax request to save account changes (for admin account) */
-function saveAdminChanges() {
-    if (!validFields()) {
-        alert("Please fill in all fields and use a password that is at least " + PWD_LEN + " characters long.");
-        return;
-    }
-    var post_url = "/users/update_pass"
-    var request_method = "PUT";
-    var main_url = "/frontend/src/staff/dashboard.html";
-    var form_data = {
-        firstName:$("#first_name").val(),
-        lastName:$("#last_name").val(),
-        pass:$("#password").val(),
-        name:$("#username").val(),
-        id:uid
-    };
-    console.log(form_data);
-
-    $.ajax({
-        type: request_method,
-        contentType: "application/json",
-        url: post_url,
-        data: JSON.stringify(form_data),
-        dataType: 'json',
-        cache: false,
-        timeout: 60000,
-        success: function (data) {
-            console.log("success");
-            $(location).attr("href", main_url);
-        },
-        error: function (e) {
-            console.log("fail");
-            console.log(e);
-            alert("Your profile failed to update, please try again.")
-        }
-    });
-    return;
+    })
 };
 
 /** Logout by deleting uid in localstorage and authentication token */
